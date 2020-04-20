2dc7d9f92525f11d35bbc493e81e61f338ef9761
Tika Tsiklauri <tika1114@gmail.com>
2020-04-20 15:01:33 -0400

    frontent auth

---
+++ frontend/src/components/splash/splash.js
@@ -0,0 +1,6 @@
+// import { connect } from "react-redux";
+// import { login, signup } from '../../actions/session_actions';
+
+// const msp = (state) => ({
+//     errors: state.entities.errors.session
+// })
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 import jwt_decode from 'jwt-decode';
+import axios from 'axios';
+import { setAuthToken } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
