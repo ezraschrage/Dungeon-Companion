87b1b114ee15d79d7b70d73815d43680968fc9a4
ezschrage <ezschrage@gmail.com>
2020-04-20 09:24:36 -0400

    Modify index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Root from './components/root';
+import configureStore from './store/store';
+import jwt_decode from 'jwt-decode';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+document.addEventListener('DOMContentLoaded', () => {
+  let store;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  const root = document.getElementById('root');
+
+  ReactDOM.render(<Root store={store} />, root);
+});
