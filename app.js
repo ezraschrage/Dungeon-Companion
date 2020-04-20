53bf09b7c44eb4386cfa41345a8829374e848e04
Rory Phair <roryphair@gmail.com>
2020-04-20 11:13:46 -0400

    made backend User Auth

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const users = require("./routes/api/users");
+
+
 
 mongoose
-    .connect(db, { useNewUrlParser: true })
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true  })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-const port = process.env.PORT || 5000;
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
+app.get("/", (req, res) => res.send("dungeons stuff"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
