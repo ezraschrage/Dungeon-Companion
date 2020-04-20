94fdb3ae3e2322e17623db718553083ca06cb469
ezschrage <ezschrage@gmail.com>
2020-04-20 14:57:40 -0400

    Monster rules from SRD API

---
+++ frontend/src/actions/rules_actions.js
@@ -0,0 +1,32 @@
+import * as APIUtil from '../util/rules_api_util';
+
+export const RECEIVE_MONSTERS = "RECEIVE_MONSTERS";
+export const RECEIVE_MONSTER = "RECEIVE_MONSTER"
+
+export const receiveMonsters = monsters => ({
+    type: RECEIVE_MONSTERS,
+    monsters
+})
+
+export const receiveMonster = monster => ({
+    type: RECEIVE_MONSTER,
+    monster
+})
+
+export const fetchMonsters = () => dispatch => (
+    APIUtil.getMonsters().then(monsters => (
+        dispatch(receiveMonsters(monsters))
+    ))
+);
+
+export const fetchMonster = monster => dispatch => (
+    APIUtil.getMonster(monster).then(monster => (
+        dispatch(receiveMonster(monster))
+    ))
+);
+
+export const searchMonsters = monster => dispatch => (
+    APIUtil.searchMonster(monster).then(monster => (
+        dispatch(receiveMonster(monster))
+    ))
+);
