586c17b803f6e51c45392f3e07d76ff058b91b5e
Rory Phair <roryphair@gmail.com>
2020-04-21 13:56:10 -0400

    make routes and models for games backend

--- routes/api/characters.js
+++ routes/api/characters.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const passport = require('passport');
 const Character = require('../../models/Character');
 const validateCharacterInput = require('../../validation/characters');
