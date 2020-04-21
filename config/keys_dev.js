2cfa1e8e68191008a2e4772d87edc440911ceb16
Rory Phair <roryphair@gmail.com>
2020-04-21 15:19:14 -0400

    add backend search for users, games, characters

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+ 
+module.exports = {
+    mongoURI: 'mongodb+srv://ezraschrage:wcHUTuBuhX3vKTgH@cluster0-y5lmb.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'Rh8qsXTdCgCkc8pR'
+    }
+    
