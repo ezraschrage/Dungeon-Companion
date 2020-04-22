a4fd7fc1dd9f40476a64efad48280fdcaa9579ac
ezschrage <ezschrage@gmail.com>
2020-04-22 13:41:16 -0400

    Receive User Sign In Comment

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -4,7 +4,7 @@ import jwt_decode from 'jwt-decode';
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
-export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
+// export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
 
 export const logoutUser = () => ({
     type: RECEIVE_USER_LOGOUT
@@ -26,9 +26,9 @@ export const receiveErrors = errors => ({
     errors
 });
 
-export const receiveUserSignIn = () => ({
-    type: RECEIVE_USER_SIGN_IN
-});
+// export const receiveUserSignIn = () => ({
+//     type: RECEIVE_USER_SIGN_IN
+// });
 
 export const signup = user => dispatch => (
     APIUtil.signup(user).then(res => {
