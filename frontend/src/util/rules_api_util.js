5514ea884b738a673ef30983b1ac9529b08cb750
ezschrage <ezschrage@gmail.com>
2020-04-24 15:15:22 -0400

    Herkou more

--- frontend/src/util/rules_api_util.js
+++ frontend/src/util/rules_api_util.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const getMonsters = () => {
-    return axios.get('http://www.dnd5eapi.co/api/monsters')
+    return axios.get('https://www.dnd5eapi.co/api/monsters')
 }
 
 export const getMonster = monster => {
-    return axios.get(`http://www.dnd5eapi.co/api/monsters/${monster}`)
+    return axios.get(`https://www.dnd5eapi.co/api/monsters/${monster}`)
 }
 
 export const searchMonster = monster => {
-    return axios.get(`http://www.dnd5eapi.co/api/monsters?name=${monster}`)
+    return axios.get(`https://www.dnd5eapi.co/api/monsters?name=${monster}`)
 }
